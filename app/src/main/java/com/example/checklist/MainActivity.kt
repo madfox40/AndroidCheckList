4cdc0495992979a87872c738b80b072dc82eb576
Victor Herguedas <victor.herguedas@edu.uah.es>
2022-12-18 15:37:30 +0100

    Remove buttons, states, option to add element, edditable texts

--- app/src/main/java/com/example/checklist/MainActivity.kt
+++ app/src/main/java/com/example/checklist/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.checklist
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.text.Editable
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators
-import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
@@ -14,11 +14,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,33 +36,38 @@ import com.example.checklist.ui.theme.CheckListTheme
 
 
 class MainActivity : AppCompatActivity() {
+    val data = mutableListOf<MyItems>(
+        MyItems(
+            name = "Hola",
+            body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat leo vel nisi luctus, a fringilla nisl pulvinar. Praesent varius, nibh sed placerat sodales, ante sem sodales ipsum, at dignissim arcu urna non est. Aenean in rutrum diam. Sed nec mollis felis. In in tempus orci. Ut non aliquet nisl."
+        ),
+        MyItems(
+            name = "Muy Buenas",
+            body = "Praesent quis sagittis nisi, at euismod metus. Nunc faucibus lectus erat, quis porta lectus tincidunt eu. Nullam luctus risus eget viverra ultricies. Maecenas congue, eros ac bibendum placerat, nibh diam semper elit, at sagittis tortor justo id velit. Integer vel est vel felis varius placerat. Etiam sollicitudin arcu diam, non auctor lectus ullamcorper sed."
+        ),
+        MyItems(name = "Hola", body = "Esto es una prueba jajaj saludos"),
+        MyItems(
+            name = "Que tal",
+            body = "Nullam sed posuere dolor, at vehicula felis. Aliquam erat volutpat. Duis semper, tellus sed cursus imperdiet, mi mi suscipit eros, a pulvinar leo purus at tortor. Vivamus ac imperdiet leo. Aliquam sollicitudin luctus tellus, vitae tempus nibh elementum et. Integer sit amet mollis purus. In hac habitasse platea dictumst."
+        ),
+        MyItems(name = "Caracola", body = "Esto es una prueg"),
+        MyItems(name = "Pepito", body = "A ver a ver"),
+        MyItems(name = "Adrianita", body = "A ver a ver"),
+        MyItems(name = "Adrianita", body = "A ver a ver")
+    ).toMutableStateList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContent {
-            var data = listOf<MyItems>(
-                MyItems(
-                    name = "Hola",
-                    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat leo vel nisi luctus, a fringilla nisl pulvinar. Praesent varius, nibh sed placerat sodales, ante sem sodales ipsum, at dignissim arcu urna non est. Aenean in rutrum diam. Sed nec mollis felis. In in tempus orci. Ut non aliquet nisl."
-                ),
-                MyItems(
-                    name = "Muy Buenas",
-                    body = "Praesent quis sagittis nisi, at euismod metus. Nunc faucibus lectus erat, quis porta lectus tincidunt eu. Nullam luctus risus eget viverra ultricies. Maecenas congue, eros ac bibendum placerat, nibh diam semper elit, at sagittis tortor justo id velit. Integer vel est vel felis varius placerat. Etiam sollicitudin arcu diam, non auctor lectus ullamcorper sed."
-                ),
-                MyItems(name = "Hola", body = "Esto es una prueba jajaj saludos"),
-                MyItems(
-                    name = "Que tal",
-                    body = "Nullam sed posuere dolor, at vehicula felis. Aliquam erat volutpat. Duis semper, tellus sed cursus imperdiet, mi mi suscipit eros, a pulvinar leo purus at tortor. Vivamus ac imperdiet leo. Aliquam sollicitudin luctus tellus, vitae tempus nibh elementum et. Integer sit amet mollis purus. In hac habitasse platea dictumst."
-                ),
-                MyItems(name = "Caracola", body = "Esto es una prueg"),
-                MyItems(name = "Pepito", body = "A ver a ver"),
-                MyItems(name = "Adrianita", body = "A ver a ver")
-            )
-            Auth()
+
+            //Auth()
             //AppNavigation()
             CheckListTheme {
 
-                //MyComposition(checkItems = data)
+                MyCheckList()
             }
         }
         //Setup
@@ -101,18 +108,62 @@ class MainActivity : AppCompatActivity() {
 
     data class MyItems(val name: String, val body: String)
 
+
+
     @Composable
-    fun MyComponent(checkItem: MyItems) {
+    fun MyCheckListItem(label: String, onClose: () -> Unit) {
+        var checkedState by rememberSaveable { mutableStateOf(false) }
+
+        MyCheckListItem(
+            label = label,
+            checked = checkedState,
+            onCheckedChange = { newValue -> checkedState = newValue },
+            onClose = onClose
+        )
+    }
+
+    @Composable
+    fun MyCheckListItem(
+        label: String,
+        checked:Boolean = false,
+        onCheckedChange: (Boolean) -> Unit = {},
+        onClose: () -> Unit ,
+    ) {
         Row(
             modifier = Modifier
                 .background(MaterialTheme.colors.background)
                 .padding(8.dp)
                 .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            MyImage()
-            MyTexts(checkItem)
+            //MyImage()
+            Checkbox(checked = checked, onCheckedChange)
+
+            var text by rememberSaveable {
+                mutableStateOf(label)
+            }
+            TextField(colors= TextFieldDefaults.textFieldColors(
+                backgroundColor= Color.White, textColor = MaterialTheme.colors.primary,
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+                value = text, onValueChange = {text = it},
+                textStyle = MaterialTheme.typography.subtitle1)
+            //MyText(text = label, color = MaterialTheme.colors.primary, style = MaterialTheme.typography.subtitle1)
+            Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End ) {
+                IconButton(onClick = onClose) {
+                    Icon(Icons.Filled.Close, contentDescription = "Close", tint = MaterialTheme.colors.primaryVariant)
+                }
+            }
+
+
+            //MyTexts(checkItem)
+
         }
     }
+
 
     @Composable
     fun MyTexts(checkItem: MyItems) {
@@ -175,10 +226,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     @Composable
-    fun MyComposition(checkItems: List<MyItems>) {
+    fun MyCheckList(
+        checkItems: SnapshotStateList<MyItems> = remember { data }
+    ) {
         Scaffold(
             floatingActionButton = {
-                FloatingActionButton(onClick = { /*TODO*/ }) {
+                FloatingActionButton(onClick = { data.add(MyItems("Hola","adios"))}) {
 
                 }
             },
@@ -188,12 +241,25 @@ class MainActivity : AppCompatActivity() {
             }
         ) {
             // Screen content
-            LazyColumn()
-            {
-                items(checkItems) { checkItem ->
-                    MyComponent(checkItem = checkItem)
+            Column() {
+                LazyColumn(Modifier.padding(bottom = 0.dp))
+                {
+                    items(checkItems) { checkItem ->
+                        MyCheckListItem(label = checkItem.name, onClose = {checkItems.remove(checkItem)})
+                    }
                 }
+                /*Button(modifier = Modifier.padding(8.dp),onClick = { checkItems.add(MyItems(name = "hola", body = "adios")) }) {
+                    Row() {
+                        Icon(Icons.Filled.Add, contentDescription = "Close", tint = MaterialTheme.colors.primaryVariant)
+                        Spacer(modifier = Modifier.padding(4.dp))
+
+                        Text(text = "Add New Item")
+                    }
+                }*/
             }
+
+                //.padding(8.dp)
+
         }
     }
 
@@ -214,7 +280,7 @@ class MainActivity : AppCompatActivity() {
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(8.dp))
-            
+
             Button(onClick = {
                 if(auth){
                     auth = false
@@ -227,12 +293,6 @@ class MainActivity : AppCompatActivity() {
                 Text(if (auth) "Cerrar" else "Authenticate")
             }
         }
-    }
-
-    @Preview(showSystemUi = true)
-    @Composable
-    fun DefaultPreviewAuth(){
-        Auth()
     }
 
     @Preview(showSystemUi = true)
@@ -273,11 +333,17 @@ class MainActivity : AppCompatActivity() {
             MyItems(name = "Pepito", body = "A ver a ver"),
             MyItems(name = "Adrianita", body = "A ver a ver"),
 
-        )
+            )
 
         CheckListTheme() {
-            MyComposition(checkItems = data)
+            MyCheckList()
         }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun DefaultPreviewAuth(){
+        Auth()
     }
 
     @Preview(showBackground = true)
--- app/src/main/java/com/example/checklist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/checklist/ui/theme/Theme.kt
@@ -5,18 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200,
-    onBackground = black
+    onBackground = black,
+
+
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = black,
+    secondary = Color.Red,
     onBackground = white
 
     /* Other default colors to override
